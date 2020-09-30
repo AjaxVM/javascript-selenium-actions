7bf14b08bfa9b7734b64ff7489e886e89b3baf36
Matthew Roe <matt.roe@techstars.com>
2020-09-30 07:02:10 -0500

    remove comments

--- tests/e2e/selenium-env.js
+++ tests/e2e/selenium-env.js
@@ -14,32 +14,12 @@ class CustomEnvironment extends NodeEnvironment {
       detached: true
     })
     this.serverProc.unref()
-    // await someSetupTasks(this.testPath);
-
-    // this.global.someGlobalObject = createGlobalObject();
-
-    // // Will trigger if docblock contains @my-custom-pragma my-pragma-value
-    // if (this.docblockPragmas['my-custom-pragma'] === 'my-pragma-value') {
-    //   // ...
-    // }
   }
 
   async teardown() {
-    // this.global.someGlobalObject = destroyGlobalObject();
-    // await someTeardownTasks();
     this.serverProc && this.serverProc.kill('SIGINT')
     await super.teardown();
   }
-
-  // runScript(script) {
-  //   return super.runScript(script);
-  // }
-
-  // async handleTestEvent(event, state) {
-  //   if (event.name === 'test_start') {
-  //     // ...
-  //   }
-  // }
 }
 
 module.exports = CustomEnvironment;
